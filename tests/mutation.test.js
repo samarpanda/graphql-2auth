c6afa895a638950b8649d575e4f949f7ff86813d
Samar Panda <er.samarpanda@gmail.com>
2020-02-25 09:29:06 +0530

    testing mutations & resolvers in graphql

---
+++ tests/mutation.test.js
@@ -0,0 +1,32 @@
+const gql = require('graphql-tag');
+const createTestServer = require('./helper');
+
+const CREATE_POST = gql`
+  mutation {
+    createPost(input: { message: "hello" }) {
+      message
+    }
+  }
+`;
+
+describe('mutations', () => {
+  test('createPost', async () => {
+    const { mutate } = createTestServer({
+      user: { id: 1 },
+      models: {
+        Post: {
+          // createOne: jest.fn(() => [{ message: 'hello' }])
+          createOne() {
+            return {
+              message: 'hello'
+            };
+          }
+        },
+        user: { id: 1 }
+      }
+    });
+
+    const res = await mutate({ query: CREATE_POST });
+    expect(res).toMatchSnapshot();
+  });
+});
