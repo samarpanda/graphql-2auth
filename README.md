03b4bfc781efeeb7b8f8db375cf2a932108b28d1
Samar <er.samarpanda@gmail.com>
2020-08-08 23:12:46 +0530

    packages upgraded & project readme.md added

---
+++ README.md
@@ -0,0 +1,41 @@
+# GraphQL Authentication & Authorization
+
+> GraphQL-2Auth
+
+```cmd
+# Install dependencies
+$ yarn install
+
+# Run project
+$ yarn start
+
+# Run project as dev with file watch
+$ yarn dev
+
+# Trigger test
+$ yarn test
+```
+
+## Things done in this project:
+
+1. GraphQL API
+1. Authentication & Authorization using JWT Token
+1. Test using jest
+
+## Signup flow
+
+> using playground screenshot
+
+![Signup-Screenshot](https://i.imgur.com/ur3cM6Q.png)
+
+## SignIn flow
+
+> using playground screenshot
+
+![SignIn-Screenshot](https://i.imgur.com/K63nfxP.png)
+
+## Fetch user /me with authorization header
+
+> using playground screenshot
+
+![Fetch-User-Screenshot](https://i.imgur.com/HnLiPKM.png)
--- src/db/models.js
+++ src/db/models.js
@@ -1,24 +1,16 @@
-const nano = require('nanoid');
+const nano = require('nanoid').nanoid;
 
 const createModel = (db, table) => ({
   findOne(filter = {}) {
     if (!filter) {
-      db.get(table)
-        .head()
-        .value();
+      db.get(table).head().value();
     }
 
-    return db
-      .get(table)
-      .find(filter)
-      .value();
+    return db.get(table).find(filter).value();
   },
   findMany(filter) {
     if (!filter) {
-      return db
-        .get(table)
-        .orderBy(['createdAt'], ['desc'])
-        .value();
+      return db.get(table).orderBy(['createdAt'], ['desc']).value();
     }
 
     return db
@@ -28,41 +20,24 @@ const createModel = (db, table) => ({
       .value();
   },
   updateOne(filter, update) {
-    const match = db
-      .get(table)
-      .find(filter)
-      .value();
+    const match = db.get(table).find(filter).value();
 
-    db.get(table)
-      .find(filter)
-      .assign(update)
-      .write();
+    db.get(table).find(filter).assign(update).write();
 
-    return db
-      .get(table)
-      .find({ id: match.id })
-      .value();
+    return db.get(table).find({ id: match.id }).value();
   },
   remove(filter) {
-    return db
-      .get(table)
-      .remove(filter)
-      .write();
+    return db.get(table).remove(filter).write();
   },
   createOne(fields) {
     const item = { ...fields, createdAt: Date.now(), id: nano() };
-    db.get(table)
-      .push(item)
-      .write();
+    db.get(table).push(item).write();
 
-    return db
-      .get(table)
-      .find({ id: item.id })
-      .value();
+    return db.get(table).find({ id: item.id }).value();
   },
   createMany(toCreate) {
     const manyToCreate = (Array.isArray(toCreate) ? toCreate : [toCreate]).map(
-      item => ({
+      (item) => ({
         ...item,
         createdAt: Date.now(),
         id: nano()
